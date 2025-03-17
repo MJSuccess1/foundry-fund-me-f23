34b59235e8a53ad63273827be43e8e3510b3ebba
Success MJ <152626084+MJSuccess1@users.noreply.github.com>
2025-03-17 15:28:52 +0200

    HelperConfig.s.sol

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -6,13 +6,9 @@ import {Script} from "forge-std/Script.sol";
 import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
 
-
 contract HelperConfig is Script {
-//if we on local anvil, deploy mocks
-//otherwise  grab existing address from live network
 
-    NetworkConfig public activeNetworkConfig; //will set this to the onewe are on
-
+    NetworkConfig public activeNetworkConfig; 
     uint8 public constant DECIMALS = 8;
     int256 public constant INITIAL_PRICE = 2000e8;
 
@@ -21,37 +17,34 @@ contract HelperConfig is Script {
     }
 
     constructor() {
-        if (block.chainid == 11155111) { //11155111 is id Sepolia 
-            activeNetworkConfig = getSepoliaETHConfig();//we activating the sepoliaconfig if we in sep chain
+        if (block.chainid == 11155111) { //11155111 is Sepolia's chsin Id
+            activeNetworkConfig = getSepoliaETHConfig(); //we are activating the sepoliaconfig if we are on the Sepolia chain
         } else if (block.chainid == 1) {  
             activeNetworkConfig = getMainnetETHConfig();
         } else {
-            activeNetworkConfig = getOrCreateAnvilETHConfig();//otherwise use anvil config
+            activeNetworkConfig = getOrCreateAnvilETHConfig(); 
         }  
-        }
+    }
     
 
         
-    
 
-    function getSepoliaETHConfig() public pure returns (NetworkConfig memory) { //this whole thing is to grab existing address and use it
+    function getSepoliaETHConfig() public pure returns (NetworkConfig memory) { 
         NetworkConfig memory sepoliaConfig = NetworkConfig({
             priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306 });
         return sepoliaConfig;
     }
 
-    function getMainnetETHConfig() public pure returns (NetworkConfig memory) { //this whole thing is to grab existing address and use it
+    function getMainnetETHConfig() public pure returns (NetworkConfig memory) { 
         NetworkConfig memory ethConfig = NetworkConfig({
             priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306 });
         return ethConfig; //a way to get ethConfig
     }
 
-    function getOrCreateAnvilETHConfig() public returns (NetworkConfig memory) {//use meory keywords xos its a special object
-
+    function getOrCreateAnvilETHConfig() public returns (NetworkConfig memory) {
         if (activeNetworkConfig.priceFeed != address(0)) {//if its  not default or 0 addres therefore we hv alresady set it
             return activeNetworkConfig;
         }
-       
 
         // NetworkConfig memory anvilConfig = NetworkConfig({
         // }
@@ -65,7 +58,7 @@ contract HelperConfig is Script {
             priceFeed: address(mockPriceFeed) });
             return anvilConfig;
          }   
-    }
+}
 
 
 
