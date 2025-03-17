d9a11cd3c821b033219619fd0a376631b5362d81
Success MJ <152626084+MJSuccess1@users.noreply.github.com>
2025-03-17 15:19:07 +0200

    Update DeployFundMe.s.sol

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -11,15 +11,12 @@ contract DeployFundMe is Script {
     
     function run() external returns (FundMe) {
 
-        //before vm.broadcat == not real tx(simulate environment)
         HelperConfig helperConfig = new HelperConfig(); //create new helperconfig 
         address ethUsdPriceFeed = helperConfig.activeNetworkConfig();//get address like this
 
-        //anything in here will be broadcast === real tx
         vm.startBroadcast();
         FundMe fundMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
         return fundMe;
         }   
-
     }
